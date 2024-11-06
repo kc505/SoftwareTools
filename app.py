174adab4c4981d384e8f19ff779263ca1fdf3ca0
kc505 <lydiekasey123@gmail.com>
2024-11-06 17:25:19 +0100

    continuous intergration

--- app.py
+++ app.py
@@ -1,23 +1,12 @@
 from flask import Flask, jsonify
-import unittest
 
 app = Flask(__name__)
 
+
 @app.route('/')
 def home():
- return jsonify(message="Hello level 400 FET, Quality Assurance")
+    return jsonify(message="Hello level 400 FET, Quality Assurance!")
 
-class TestMyApp(unittest.TestCase):
- def setUp(self):
-  self.app = app.test_client()
-  
- def test_home_route(self):
-   response = self.app.get('/')
-   self.assertEqual(response.app.get('/'))
-   self.assertEqual(response.json, {'message': 'Hello level 400 FET, Quality Assurance!'})
-   
-   
-   
+
 if __name__ == '__main__':
- app.run(debug=True)
- unittest.main()
+    app.run(debug=True)
