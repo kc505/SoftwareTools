fbeb7395cee036111220709b061c16b518198f4c
kc505 <lydiekasey123@gmail.com>
2024-11-13 15:37:28 +0100

    Integration

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ from flask import Flask, jsonify
 app = Flask(__name__)
 
 
-@app.route('')
+@app.route('/my_project_env')
 def home():
     return jsonify(message="Hello level 400 FET, Quality Assurance!")
 
