db583eb6dff8ccc781473069d11b8ffe7f58e562
kc505 <lydiekasey123@gmail.com>
2024-11-15 14:28:09 +0100

    Continuous Integration

--- app.py
+++ app.py
@@ -1,13 +1,15 @@
+# app.py
 from flask import Flask, jsonify
 
 
 app = Flask(__name__)
 
 
-@app.route('/my_project_env')
+@app.route('/')
 def home():
     return jsonify(message="Hello level 400 FET, Quality Assurance!")
 
 
 if __name__ == '__main__':
     app.run(debug=True)
+    
